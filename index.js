d555b29fea04bd86ec4efc452eb3a3fb55f1d719
Tonie <victornwanochi@gmail.com>
2022-08-23 01:23:02 +0100

    Initial-commit

--- index.js
+++ index.js
@@ -26,11 +26,6 @@ app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
 app.use("/api/checkout", stripeRoute);
 
-app.use(express.static(path.join(__dirname, "/client/build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/client/build", "index.html"));
-});
-
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Server is up and running`);
 });
